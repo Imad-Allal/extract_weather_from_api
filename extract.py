3cf7500aa4a1eb1ffbff4c9c7a0cc10f51b5e050
The Trustor <imad.allal@outlook.com>
2023-06-10 21:17:33 +0100

    Updating the Dockerfil and fixing the crontab

--- extract.py
+++ extract.py
@@ -6,16 +6,14 @@ import requests
 
 def get_list_of_cities():
     # read the file cities.json and returns its content.
-    # Example: this function should return ["Algiers", "Batna", "Tamanrasset"]
     file = open('cities.json')
     data = json.load(file)
 
     return data
 
 def get_lat_lon(city):
-    # get latitude and longitude data of cities in Algeria 
+    # getting latitude and longitude data of cities in Algeria 
     # using the API documented here: https://nominatim.org/release-docs/latest/api/Search/
-    # Example: this function should return 36.6875, 3.125
     url = f'https://nominatim.openstreetmap.org/search?city={city}&country=algeria&format=json'
     response = requests.get(url)
     data = json.loads(response.text)
@@ -25,12 +23,9 @@ def get_lat_lon(city):
 
     return lat, lon
 
-    #if response.json().address.city == city:
-
-    #     return response.json().lat, response.json().lon
 
 def get_current_weather(lat, lon):
-    # get current weather data at (latitude, longitude)
+    # getting current weather data at (latitude, longitude)
     # using the API documented here: https://open-meteo.com/
     url = f'https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true'
     response = requests.get(url)
--- transform.py
+++ transform.py
@@ -8,17 +8,12 @@ import folium
 
 def get_all_filenames():
     # this function should return a list of filenames
-    # you should look at glob library
-    # Example: it should return ["raw_data_1686027160.json", "raw_data_1686027165.json", "raw_data_1686027170.json"]
     files = glob.glob("raw_data_*")
     return files
 
 def transform_one_file(filename):
-    # this function should return a list of dictionaries
-    # each dictionnary should look like that: { "city": city, "latitude": latitude, "longitude": longitude , "temperature": tempature, "time": time}
     file = open(filename)
     data = json.load(file)
-    #{'Algiers': 24.1, 'Annaba': 23.0, 'Batna': 19.0, 'Tamanrasset': 32.2, 'Tlemcen': 25.1}
 
     listCities = []
     
@@ -42,7 +37,7 @@ def merge_all_files_in_pandas_df(files):
     return df
 
 def drop_duplicates(df_):
-    # drop duplicated rows using a function of pandas.DataFrame
+    # dropping duplicated rows using a function of pandas.DataFrame
     return df_.drop_duplicates()
 
 
